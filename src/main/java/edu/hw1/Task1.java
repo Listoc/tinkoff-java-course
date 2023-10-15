ea4e4e5364a18b8666f40ff816aeb8516b337349
Listoc <mestis0408@gmail.com>
2023-10-15 14:22:22 +0300

    Fix Task1

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -8,37 +8,27 @@ public class Task1 {
             return -1;
         }
 
+        int seconds;
+        int minutes;
+        int result = 0;
+
         String[] split = time.split(":");
 
         if (split.length != 2 || split[0].length() < 2 || split[1].length() != 2) {
             return -1;
         }
 
-        int seconds;
-        int minutes;
-        int result = 0;
-
-        char[] firstNumberArray = split[0].toCharArray();
-
-        for (var el : firstNumberArray) {
-            if (!Character.isDigit(el)) {
-                result = -1;
-            }
-        }
-
-        if (!Character.isDigit(split[1].charAt(0)) || !Character.isDigit(split[1].charAt(1))) {
-            result = -1;
-        }
-
-        if (result != -1) {
+        try {
             minutes = Integer.parseInt(split[0]);
             seconds = Integer.parseInt(split[1]);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
 
-            if (minutes < 0 || seconds < 0 || seconds >= SECONDS_IN_MINUTE) {
-                result = -1;
-            } else {
-                result = minutes * SECONDS_IN_MINUTE + seconds;
-            }
+        if (minutes < 0 || seconds < 0 || seconds >= SECONDS_IN_MINUTE) {
+            result = -1;
+        } else {
+            result = minutes * SECONDS_IN_MINUTE + seconds;
         }
 
         return result;
