cf4e91ebbe623a0d176dad095937ec3904ff4b84
Listoc <mestis0408@gmail.com>
2023-10-24 20:01:06 +0300

    Fix hw2/Task4

--- src/main/java/edu/hw2/task4/Task4.java
+++ src/main/java/edu/hw2/task4/Task4.java
@@ -2,12 +2,8 @@ package edu.hw2.task4;
 
 public class Task4 {
     public static CallingInfo callingInfo() {
-        try {
-            throw new Exception();
-        } catch (Exception e) {
-            StackTraceElement[] stackTrace = e.getStackTrace();
-            return new CallingInfo(stackTrace[1].getClassName(), stackTrace[1].getMethodName());
-        }
+        StackTraceElement[] stackTrace = new Exception().getStackTrace();
+        return new CallingInfo(stackTrace[1].getClassName(), stackTrace[1].getMethodName());
     }
 
     private Task4() {}
