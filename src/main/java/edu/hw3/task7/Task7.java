69e3da3a539fea79db0e706f676dc0336a788467
Listoc <mestis0408@gmail.com>
2023-10-28 16:08:27 +0300

    Fix hw3/Task7

--- src/main/java/edu/hw3/task7/Task7.java
+++ src/main/java/edu/hw3/task7/Task7.java
@@ -5,7 +5,21 @@ import java.util.Comparator;
 public class Task7 {
 
     public static <T extends Comparable<T>> Comparator<T> getNullComparator() {
-        return Comparator.nullsFirst(Comparable::compareTo);
+        return (o1, o2) -> {
+            if (o1 == null && o2 == null) {
+                return 0;
+            }
+
+            if (o1 == null) {
+                return -1;
+            }
+
+            if (o2 == null) {
+                return 1;
+            }
+
+            return o1.compareTo(o2);
+        };
     }
 
     private Task7() {}
