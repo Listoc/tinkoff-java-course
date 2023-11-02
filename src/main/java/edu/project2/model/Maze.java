49b43235fc2b482172c9dabd6fa2b226c69462ac
Listoc <mestis0408@gmail.com>
2023-11-02 23:28:47 +0300

    Delete method for DFS from Maze in project2

--- src/main/java/edu/project2/model/Maze.java
+++ src/main/java/edu/project2/model/Maze.java
@@ -20,21 +20,6 @@ public class Maze {
         }
     }
 
-    public void setMazeForDFS() {
-        for (int i = 0; i < height; ++i) {
-            for (int j = 0; j < width; ++j) {
-                if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
-                    mazeArray[i][j].setType(Cell.Type.Wall);
-                } else if (i % 2 == 1 && j % 2 == 1) {
-                    mazeArray[i][j].setType(Cell.Type.Passage);
-                } else {
-                    mazeArray[i][j].setType(Cell.Type.Wall);
-                }
-
-            }
-        }
-    }
-
     public void setAllWalls() {
         for (var arr : mazeArray) {
             for (var cell : arr) {
