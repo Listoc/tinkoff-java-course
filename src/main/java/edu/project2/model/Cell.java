6fd66a40a94e5578a5856034f9a98bac1ef73fc9
Listoc <mestis0408@gmail.com>
2023-11-02 19:56:13 +0300

    Add class for Cell of maze in project2

---
+++ src/main/java/edu/project2/model/Cell.java
@@ -0,0 +1,60 @@
+package edu.project2.model;
+
+import java.util.Objects;
+
+public class Cell {
+    private final int height;
+    private final int width;
+    private Type type;
+
+    public Cell(int height, int width, Type type) {
+        this.height = height;
+        this.width = width;
+        this.type = type;
+    }
+
+    public enum Type {
+        Wall,
+        Passage
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        Cell cell = (Cell) other;
+        return height == cell.height && width == cell.width;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(height, width);
+    }
+
+    @Override public String toString() {
+        return "Cell{" +
+            "height=" + height +
+            ", width=" + width +
+            ", type=" + type +
+            '}';
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public Type getType() {
+        return type;
+    }
+}
