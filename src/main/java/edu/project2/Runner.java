46c2272d1af7c5cabfe329c40422e3c543945514
Listoc <mestis0408@gmail.com>
2023-11-03 11:45:55 +0300

    Change imports order in Runner in project2

--- src/main/java/edu/project2/Runner.java
+++ src/main/java/edu/project2/Runner.java
@@ -1,9 +1,9 @@
 package edu.project2;
 
-import edu.project2.printer.Printer;
 import edu.project2.generator.Generator;
 import edu.project2.model.Cell;
 import edu.project2.model.Maze;
+import edu.project2.printer.Printer;
 import edu.project2.solver.Solver;
 import java.util.List;
 import java.util.Scanner;
